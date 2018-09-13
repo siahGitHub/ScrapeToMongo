259cba6a00d6f6af73b4db20e08797d6a063cc47
Siah Monk <dwaynemhinds@gmail.com>
2018-09-13 11:34:41 -0400

    more styling

--- public/app.js
+++ public/app.js
@@ -1,36 +1,24 @@
 // Grab the articles as a json
-$.getJSON("/articles", function(data) {
+$.getJSON("/articles", function (data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    //$("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].summary + data[i].link + "</p>");
-    //$("#title").append("<p>" + data[i].title+ "</p>");
-    //$("#summary").append("<p data-id='" + data[i]._id + "'>" + data[i].summary+ "</p>");
-    $("#feeds").append('<div class="panel panel-primary">'+
-    //'<div class="panel-heading"><div id="title">'+
-    '<div class="panel-heading">'+
-    '<p data-id="' + data[i]._id + '">' +
-    data[i].title+'</p></div>'+
-    '<button type="button" class="btn btn-success">Edit</button>'+
-    '<button type="button" class="btn btn-warning">Delete</button>'+
-    '<div class="panel-body"><div id="summary">'
-    + data[i].summary+
-    '</div></div>');
-    /*
-    <div class="panel-heading">
-        <div id="title"></div>
-      </div>
-      <div class="panel-body">
-        <div id="summary"></div>
-      </div>
-    </div>
-    */
+
+    $("#feeds").append('<div class="panel panel-primary">' +
+      '<div class="panel-heading">' +
+      '<p data-id="' + data[i]._id + '">' +
+      data[i].title + '</p></div>' +
+      '<button type="button" class="btn btn-success">Edit</button>' +
+      '<button type="button" class="btn btn-warning">Delete</button>' +
+      '<div class="panel-body"><div id="summary">'
+      + data[i].summary +
+      '</div></div>');
   }
 });
 
 
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
+$(document).on("click", "p", function () {
   // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
@@ -42,43 +30,41 @@ $(document).on("click", "p", function() {
     url: "/articles/" + thisId
   })
     // With that done, add the note information to the page
-    .then(function(data) {
+    .then(function (data) {
       console.log(data);
-      /*
-      $(".note-modal").append("<h2>" + data.title + "</h2>"+
-      "<input id='titleinput' name='title' >"+
-      "<textarea id='bodyinput' name='body'></textarea>"+
-      "<button data-id='" + data._id + "' id='savenote'>Save Note</button>"
-    );*/
+
       // The title of the article
       $("#notes").append(
-        '<div class="panel panel-primary">'+
-    '<div class="panel-heading">Add Note</div>'+
-    '<div class="panel-body"><div id="summary">'+
-    '<input id="titleinput" name="title" >'+
-    '<textarea id="bodyinput" name="body"></textarea>' +
-    '<div><button class="btn btn-success" data-id="' + data._id + '" id="savenote">Save Note</button>'+
-    '</div></div></div>');
-      //"<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      //$("#notes").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
-      //$("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      //$("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+        '<div class="panel panel-primary">' +
+        '<div class="panel-heading">Add Note</div>' +
+        '<div class="panel-body"><div id="summary">' +
+        '<input id="titleinput" name="title" >' +
+        '<textarea id="bodyinput" name="body"></textarea>' +
+        '<div><button class="btn btn-success" data-id="' + data._id + '" id="savenote">Save Note</button>' +
+        '<button class="btn btn-danger" data-id="' + data._id + '" id="editnote">Edit Note</button>' +
+        '</div></div></div>');
+
 
       // If there's a note in the article
       if (data.note) {
         // Place the title of the note in the title input
         $("#titleinput").val(data.note.title);
+        $("#titleinput").prop("disabled", true);
         // Place the body of the note in the body textarea
         $("#bodyinput").val(data.note.body);
+        $("#bodyinput").prop("disabled", true);
       }
     });
 });
 
+//When you click editnote button
+$(document).on("click", "#editnote", function () {
+  $("#titleinput").prop("disabled", false);
+  $("#bodyinput").prop("disabled", false);
+});
+
 // When you click the savenote button
-$(document).on("click", "#savenote", function() {
+$(document).on("click", "#savenote", function () {
   // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
 
@@ -94,7 +80,7 @@ $(document).on("click", "#savenote", function() {
     }
   })
     // With that done
-    .then(function(data) {
+    .then(function (data) {
       // Log the response
       console.log(data);
       // Empty the notes section
@@ -107,16 +93,16 @@ $(document).on("click", "#savenote", function() {
 });
 
 // When you click the savenote button
-$(document).on("click", "#scrape", function() {
+$(document).on("click", "#scrape", function () {
   // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
     method: "GET",
     url: "/scrape"
   })
     // With that done
-    .then(function(data) {
+    .then(function (data) {
       // Log the response
-      $(".scrape-modal").append("<p>"+ data + "</p>")
+      $(".scrape-modal").append("<p>" + data + "</p>")
       console.log(data);
     });
-  });
+});
